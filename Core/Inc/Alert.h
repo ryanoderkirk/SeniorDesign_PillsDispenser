6ac828fe217aac6b6836fea48f70d5bacb61b6a6
Connor Beaven <Cmbeaven1@gmail.com>
2026-04-13 21:33:38 -0400

    add volatile

--- Core/Inc/Alert.h
+++ Core/Inc/Alert.h
@@ -1,30 +1,27 @@
 /*
  * 	Alert.h
- * 	Contains function and variable declarations pertinent to the alert system
+ * 	Contains function and variable declarations pertinent to the alert
+ * system
  *
  */
 
 #ifndef ALERT_H
 #define ALERT_H
 
-
 #include <stdint.h>
 
-typedef enum dosage_alert{
-	CLEAR,
-	ACTIVE
-}dosage_alert_t;
+typedef enum dosage_alert { CLEAR, ACTIVE } dosage_alert_t;
 
-typedef enum low_pill_alert{
-	off = 0,
-	channel1 = 1,
-	channel2 = 2,
-	channel3 = 4,
-	channel4 = 8
-}low_pill_alert_mask_t;
+typedef enum low_pill_alert {
+  off = 0,
+  channel1 = 1,
+  channel2 = 2,
+  channel3 = 4,
+  channel4 = 8
+} low_pill_alert_mask_t;
 
-extern dosage_alert_t dosageAlert;
-extern uint8_t lowPillAlert;
+extern volatile dosage_alert_t dosageAlert;
+extern volatile uint8_t lowPillAlert;
 
 dosage_alert_t checkDosageAlert();
 
--- Core/Src/Alert.c
+++ Core/Src/Alert.c
@@ -6,54 +6,54 @@
  */
 
 #include "Alert.h"
-#include "main.h"
 #include "filesystem.h"
+#include "main.h"
 #include <stdint.h>
 
 // hold alert States
 dosage_alert_t dosageAlert = CLEAR;
-uint8_t lowPillAlert = 0;
+volatile uint8_t lowPillAlert = 0;
 
 const uint8_t lowBar = 10;
-uint8_t lowPillAlertTriggered = 0;
+volatile uint8_t lowPillAlertTriggered = 0;
 
 // check if a dosage needs an alert based on hours and minutes from rtc
 // returns if the alert is active or cleared
-dosage_alert_t checkDosageAlert(){
-	Dosage_t doses[5];
-	int num_doses = readDoses(doses,5);
-	RTC_DateTypeDef date;
-	RTC_TimeTypeDef time;
-	get_rtc_typedef(&date, &time);
+dosage_alert_t checkDosageAlert() {
+  Dosage_t doses[5];
+  int num_doses = readDoses(doses, 5);
+  RTC_DateTypeDef date;
+  RTC_TimeTypeDef time;
+  get_rtc_typedef(&date, &time);
 
-	for(int i = 0; i < num_doses; ++i){
-		if(time.Hours == doses[i].hour && time.Minutes == doses[i].min){
-			dosageAlert = ACTIVE;
-			return dosageAlert;
-		}
-	}
-	return dosageAlert;
+  for (int i = 0; i < num_doses; ++i) {
+    if (time.Hours == doses[i].hour && time.Minutes == doses[i].min) {
+      dosageAlert = ACTIVE;
+      return dosageAlert;
+    }
+  }
+  return dosageAlert;
 }
 
 // check if any pill channels are low
 // returns which pill channels are low
-uint8_t checkLowPillAlert(){
-	for(int i = 1; i < 5;++i){
-		Config_t config;
-		int error = readConfig(&config,i);
-		if(error != 0){
-			continue;
-		}
-		if(config.pillCount < lowBar){
-			if(lowPillAlertTriggered & (1 << (i-1)) == 0){
-				lowPillAlert = lowPillAlert | (1 << (i-1));
-				lowPillAlertTriggered = lowPillAlertTriggered | (1 << (i-1));
-			}
-		}
-	}
-	return lowPillAlert;
+uint8_t checkLowPillAlert() {
+  for (int i = 1; i < 5; ++i) {
+    Config_t config;
+    int error = readConfig(&config, i);
+    if (error != 0) {
+      continue;
+    }
+    if (config.pillCount < lowBar) {
+      if (lowPillAlertTriggered & (1 << (i - 1)) == 0) {
+        lowPillAlert = lowPillAlert | (1 << (i - 1));
+        lowPillAlertTriggered = lowPillAlertTriggered | (1 << (i - 1));
+      }
+    }
+  }
+  return lowPillAlert;
 }
 
-void clearLowPillAlert(uint8_t channel){
-	lowPillAlertTriggered = lowPillAlertTriggered & ~(1 << (channel - 1));
+void clearLowPillAlert(uint8_t channel) {
+  lowPillAlertTriggered = lowPillAlertTriggered & ~(1 << (channel - 1));
 }
